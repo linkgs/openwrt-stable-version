94754c6da3827b0079c35888d8522ade6106cd05
linkgs <40344950+linkgs@users.noreply.github.com>
2020-12-19 16:17:47 +0800

    t01.sh
    增加关机项？

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@ git clone -b master https://github.com/firkerword/OpenClash.git package/OpenClas
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/poweroff
 # git clone https://github.com/firkerword/v2fly.git package/v2fly
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
